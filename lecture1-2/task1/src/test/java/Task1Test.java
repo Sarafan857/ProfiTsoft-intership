810aa4c7f2b8e4526a8dae812fa5b5edf12f07a9
Oleksandr Sarafannikov <64796328+Sarafan857@users.noreply.github.com>
2022-11-16 21:11:08 +0200

    Add files via upload

--- lecture1-2/task1/src/test/java/Task1Test.java
+++ lecture1-2/task1/src/test/java/Task1Test.java
@@ -1,43 +1,37 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 class Task1Test {
 
-  ArrayList<Integer> arrayIn;
-  ArrayList<Integer> arrayOut;
-  ArrayList<Integer> result;
-
-
   @Test
   public void getPositiveArrayTest() {
 
-    arrayIn = new ArrayList<>(Arrays.asList(5, 10, 8, 1, -1, 0));
-    result = Task1.getPositiveArray(arrayIn);
-    arrayOut = new ArrayList<>(Arrays.asList(5, 10, 8, 1, 0));
+    int[] arrayIn = {5, 10, 8, 1, -1, 0};
+    int[] result = Task1.getPositiveArray(arrayIn);
+    int[] arrayOut = {5, 10, 8, 1, 0};
 
-    Assertions.assertEquals(arrayOut, result);
+    Assertions.assertEquals(Arrays.toString(arrayOut), Arrays.toString(result));
   }
 
   @Test
   public void getPositiveArrayTest1() {
 
-    arrayIn = new ArrayList<>(Arrays.asList(-5, -10, -8, 0, -1, 0));
-    result = Task1.getPositiveArray(arrayIn);
-    arrayOut = new ArrayList<>(Arrays.asList(0, 0));
+    int[] arrayIn = {-5, -10, -8, 0, -1, 0};
+    int[] result = Task1.getPositiveArray(arrayIn);
+    int[] arrayOut = {0, 0};
 
-    Assertions.assertEquals(arrayOut, result);
+    Assertions.assertEquals(Arrays.toString(arrayOut), Arrays.toString(result));
   }
 
   @Test
   public void onlyNegativeElemtnts() {
 
-    arrayIn = new ArrayList<>(Arrays.asList(-5, -10, -8, -1));
-    result = Task1.getPositiveArray(arrayIn);
+    int[] arrayIn = {-5, -10, -8, -1};
+    int[] result = Task1.getPositiveArray(arrayIn);
 
-    Assertions.assertEquals(0, result.size());
+    Assertions.assertEquals(0, result.length);
   }
 
 }
